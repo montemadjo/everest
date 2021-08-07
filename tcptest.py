b1ce46cf4b21dd0345f6399640731def8273dbab
DESKTOP-TFS <bogdan.maksimovic@outlook.com>
2021-08-07 11:46:21 +0200

    testing connection to UHF reader

---
+++ tcptest.py
@@ -0,0 +1,12 @@
+import socket
+
+TCP_IP = '192.168.1.200'
+TCP_PORT = 100
+BUFFER_SIZE = 1024
+MESSAGE = bytes.fromhex('0aff0222d3')
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+print("received data:" + str(data))
