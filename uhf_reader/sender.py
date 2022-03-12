e39cabf055641b4cc1304bdaa13fb8295347a9e6
= <=>
2022-03-12 18:22:55 +0100

    ...

--- uhf_reader/sender.py
+++ uhf_reader/sender.py
@@ -6,6 +6,6 @@ class Sender:
         self.url = URL
 
     def postStadionUhfCards(self, data) -> None:
-        response = requests.post(self.url, json=data)
+        response = requests.post(self.url, json=data, timeout=4)
         if response == "ok":
             print("Data posted to cloud")
